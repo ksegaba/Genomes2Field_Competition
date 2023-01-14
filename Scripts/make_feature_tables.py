0c211ce01b6c59f3cf70f91357bc7655dd78c23d
Kenia Segura Abá <keniaesegura@gmail.com>
2023-01-14 10:45:56 -0500

    Script to make training, validation, & testing feature tables

--- Scripts/make_feature_tables.py
+++ Scripts/make_feature_tables.py
@@ -61,6 +61,10 @@ if __name__ == "__main__":
     meta.drop(["Year", "Experiment_Code", "Farm", "Comments"], axis=1, inplace=True)
 
     weather = pd.read_csv("4_Training_Weather_Data_2014_2021_newdates.csv")
+    #weather.rename(columns={"Env":"ID"}, inplace=True)
+    #weather.drop("Date", axis=1, inplace=True)
+    #weather.to_csv("4_Training_Weather_Data_2014_2021_cleaned.csv", index=False)
+    #os.system("python ../Scripts/dimension_reduction.py -path /mnt/home/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Training_Data -file 4_Training_Weather_Data_2014_2021_cleaned.csv -save 4_Training_Weather_Data_2014_2021 -alg pca")
     wpc = pd.read_csv("4_Weather_centered_PCA.csv") # weather (PCs)
     len(weather.Env.str.split('_').str[0].unique()) # check for 45 fields
     len(weather.Env.unique())
@@ -247,9 +251,47 @@ if __name__ == "__main__":
     del soil
     del df
     del cvs 
+    
+    # generate pheno.csv for rrBLUP training (forgot to do this before)
+    os.chdir("/mnt/ufs18/home-056/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Testing_Data/")
+    trait = pd.read_csv("../Training_Data/1_Training_Trait_Data_2014_2021_cleaned_no_duplicates.csv")
+    sub = trait[["ID", "Yield_Mg_ha"]]
+    l = sub.Yield_Mg_ha.quantile(0.25)
+    r = sub.Yield_Mg_ha.quantile(0.75)
+    sub["yield_class"] = "low"
+    sub.loc[sub["Yield_Mg_ha"]>=r, "yield_class"] = "high"
+    sub.loc[(sub["Yield_Mg_ha"]>l) & (sub["Yield_Mg_ha"]<r), "yield_class"] = "mid"
+    sub.to_csv("0_Master_Data/pheno.csv", index=False)
+    # now split by env
+    envs = sub.ID.str.split("-").str[0].unique() # 212/217 envs
+    all_envs = pd.read_csv('../Training_Data/env_list.txt', header=None) # 217 envs
+    all_envs[~all_envs[0].isin(envs)] # the 6 envs that were lost. idk know how
+    # TXH1-Dry_2017
+    # TXH1-Early_2017
+    # TXH1-Late_2017
+    # TXH1-Dry_2018
+    # TXH1-Early_2018
+    # TXH1-Late_2018
+    for env in envs:
+        env_sub = sub.loc[sub.ID.str.contains(env),["ID", "Yield_Mg_ha"]]
+        env_sub.insert(1, "Hybrid", env_sub.ID.str.split("-").str[1])
+        # remove duplicates
+        env_sub.drop("ID", axis=1, inplace=True)
+        env_sub = env_sub.groupby("Hybrid")["Yield_Mg_ha"].median()
+        env_sub = env_sub.to_frame()
+        env_sub.reset_index(inplace=True)
+        env_sub.to_csv(f"../Training_Data/0_Master_Data/pheno_{env}.csv", index=False)
+    # make sure that all samples in 0_MASTER_Training_tsmwe_Data_2014_2021.csv are in pheno.csv
+    training = pd.read_csv('../Training_Data/0_Master_Data/0_MASTER_Training_tsmwe_Data_2014_2021.csv')
+    pheno = pd.read_csv('../Training_Data/0_Master_Data/pheno.csv')
+    training.shape #(62115, 60)
+    pheno.shape #(87356, 3)
+    training[~training.ID.isin(pheno.ID)].shape # (0, 60) yay!
 
     import os
     import pandas as pd
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
     os.chdir("/mnt/home/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Testing_Data")
 
     # Read in data
@@ -259,49 +301,143 @@ if __name__ == "__main__":
     soil = pd.read_csv("3_Testing_Soil_Data_2022.csv")
     weather = pd.read_csv("4_Testing_Weather_Data_2022.csv")
     ec = pd.read_csv("6_Testing_EC_Data_2022.csv")
-    trait = pd.read_csv("../Training_Data/1_Training_Trait_Data_2014_2021_cleaned_no_duplicates.csv")
     
-    # generate pheno.csv for rrBLUP training
-    trait[["ID", "Yield_Mg_ha"]].to_csv("0_Master_Data/pheno.csv", index=False)
+    # remove columns with missing data from weather and meta
+    weather.isna().sum()
+    weather.drop(["GWETTOP", "GWETROOT", "GWETPROF", 
+        "ALLSKY_SFC_PAR_TOT", "ALLSKY_SFC_SW_DNI"], axis=1, inplace=True)
+    # check the 130 missing values in each columns
+    weather[weather.isna().any(axis=1)].shape # 283 rows total
+    lost = weather[weather.isna().any(axis=1)].Env.unique() # Envs that would be lost
+    keep = weather[~weather.isna().any(axis=1)].Env.unique()
+    len(lost)==len(keep) # some days of each env will be lost
+    weather.dropna(inplace=True)
+    len(weather.Env.unique()) # no entire Env was lost. Hurray!
 
-    # Do test set hybrids have trait data? No.
-    info_test = info[info.test==True]
-    info_test.shape # (4902, 8)
-    len(info_test.Hybrid.unique()) # 43
-    len(info_test.Env.unique()) # 216
+    meta.drop(["Year", "Experiment_Code", "Farm", "Comments"], axis=1, inplace=True)
+    trt = pd.get_dummies(meta.Treatment) # one-hot encode meta columns
+    crp = pd.get_dummies(meta.Previous_Crop)
+    til = pd.get_dummies(meta["Pre-plant_tillage_method(s)"])
+    meta.drop(["Treatment", "Previous_Crop", "Pre-plant_tillage_method(s)"], 
+        axis=1, inplace=True)
+    meta = pd.concat([meta, trt, crp, til], axis=1)
+    meta.to_csv("2_Testing_Meta_Data_2022_cleaned_one-hot_encoded.csv")
+
+    # run pca of weather and ec
+    weather.rename(columns={"Env":"ID"}, inplace=True)
+    #weather.drop(["Date"], axis=1, inplace=True)
+    weather.to_csv("4_Testing_Weather_Data_2022_cleaned.csv", index=False) # Huan further processed this later
+    os.system("python ../Scripts/dimension_reduction.py -path /mnt/home/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Testing_Data -file 4_Testing_Weather_Data_2022_T_coulumNoNA.csv -save 4_Testing_Weather_Data_2022_T_coulumNoNA -alg pca")
+    
+    ec.rename(columns={"Env":"ID"}, inplace=True)
+    ec.to_csv("6_Testing_EC_Data_2022_cleaned.csv", index=False)
+    os.system("python ../Scripts/dimension_reduction.py -path /mnt/home/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Testing_Data -file 6_Testing_EC_Data_2022_cleaned.csv -save 6_Testing_EC_Data_2022 -alg pca")
+
+    # Merge meta, weather (PCs), and ec (PCs)
+    # wpc = pd.read_csv("4_Testing_Weather_Data_2022_centered_PCA.csv") # this is just a place holder
+    # wpc = wpc.groupby("ID")[['0', '1', '2', '3', '4', '5']].median() # this is just a place holder until huan runs here pipeline to get pca of weather dates as features
+    # wpc.reset_index(inplace=True) # place holder
+    wpc = pd.read_csv("4_Testing_Weather_Data_2022_T_coulumNoNA_centered_PCA.csv") # this is the correct dataset
+    ecpc = pd.read_csv("6_Testing_EC_Data_2022_centered_PCA.csv")
+    wpc.columns = ["Env", "WPCA0", "WPCA1", "WPCA2", "WPCA3", "WPCA4", "WPCA5", 'WPCA6', 'WPCA7', 'WPCA8', 'WPCA9', 'WPCA10', 'WPCA11', 'WPCA12', 'WPCA13', 'WPCA14', 'WPCA15', 'WPCA16']
+    ecpc.columns = ["Env", "ECPCA0", "ECPCA1", "ECPCA2", "ECPCA3", "ECPCA4", "ECPCA5", "ECPCA6", "ECPCA7"]
+    
+    meta = pd.read_csv("2_Testing_Meta_Data_2022_cleaned_one-hot_encoded.csv")
+    meta.drop("Unnamed: 0", axis=1, inplace=True)
+    meta.set_index("Env", inplace=True)
+    wpc.set_index("Env", inplace=True)
+    ecpc.set_index("Env", inplace=True)
+    mwe = pd.concat([meta, wpc, ecpc], axis=1, join="outer")
+    mwe.isna().sum() # check missing values
+    mwe.drop(["City", "Field", "Trial_ID (Assigned by collaborator for internal reference)",
+        "Soil_Taxonomic_ID and horizon description, if known", 
+        "In-season_tillage_method(s)", "System_Determining_Moisture",
+        "Pounds_Needed_Soil_Moisture", "Latitude_of_Field_Corner_#2 (lower right)",
+        "Cardinal_Heading_Pass_1", "Plot_Area_ha",
+        "Weather_Station_Serial_Number (Last four digits, e.g. m2700s#####)", 
+        "Date_weather_station_placed", "Date_weather_station_removed", 
+        "Issue/comment_#1", "Issue/comment_#2", "Issue/comment_#3",
+        "Issue/comment_#4", "Issue/comment_#5", "Issue/comment_#6", 
+        "Date_Planted"], axis=1, inplace=True)
+    mwe.isna().sum()
+    mwe = mwe.dropna()
+    len(mwe.index.str.split("-").str[0].unique()) # 17 envs
+    len(mwe.index.str.split("_").str[0].unique()) # 17 fields
+    # mwe.to_csv("2_Testing_Meta+Weather+EC_Data_2022_PLACEHOLDER.csv") # change index to ID manually in vim
+    mwe.to_csv("2_Testing_Meta+Weather+EC_Data_2022.csv") # change index to ID manually in vim
+    
+    # Merge template, soil, mwe
+    template.insert(0, "ID", template.Env.str.cat(template.Hybrid, "-"))
+    soil["Date Received"] = pd.to_datetime(soil["Date Received"]).dt.strftime("%Y-%m-%d")
+    soil["Date Reported"] = pd.to_datetime(soil["Date Reported"]).dt.strftime("%Y-%m-%d")
+    soil.to_csv("3_Testing_Soil_Data_2022_newdates.csv", index=False)
+    ts = pd.merge(template[["ID", "Env"]], soil, how="left", on="Env")
+    ts.isna().sum()
+    mwe.reset_index(inplace=True)
+    mwe.rename(columns={"index":"Env"}, inplace=True)
+    out = pd.merge(ts, mwe, how="left", on="Env") # cannot do inner bc we need to keep all the hybrids!
+    out.isna().sum() # there are a lot of NAs
+    ts.isna().sum() # no Env are missing, but WPCA and ECPCA have missing data in out
+    mwe.isna().sum() # no Env are missing
+    len(weather.Env.unique()) # 26
     len(template.Env.unique()) # 26
-    len(template.Hybrid.unique()) # 548
-    IDs = template.Env.str.cat(template.Hybrid, "-").to_frame() # get Env-Hybrid IDs
-    IDs.rename(columns={"Env":"ID"}, inplace=True)
-    trait_test = IDs.merge(trait, how="inner", on="ID")
-    trait_test.shape # (0, 11)
-
-    # useful commands: len(), df.shape, df.head()
-    # Take averages or medians across envs and hybrids for each column in the training set trait data
-    # to get putative test set trait values
-    # use trait dataset
-    # consider Field and Hybrid (not year)
-    trait["Field"] = trait.ID.str.split("_").str[0]
-    trait["Hybrid"] = trait.ID.str.split("-").str[1]
-    # first, subset trait by field and hybrid that are in template
-    trait.shape
-    template.shape
-    template["Field"] = template.Env.str.split("_").str[0]
-    sub = trait[(trait.Field.isin(template.Field)) & (trait.Hybrid.isin(template.Hybrid))]
-    sub.shape
-    # second, take medians across all the columns 
-    out = sub.groupby(["Field", "Hybrid"])[['Plot_Area_ha', 'Pollen_DAP_days', 'Silk_DAP_days',
-       'Plant_Height_cm', 'Ear_Height_cm', 'Yield_Mg_ha', 'Grain_Moisture',
-       'Twt_kg_m3', 'Age']].median()
-    out.shape
-
-    # third, merge with meta, weather (PCA), soil, and ec (PCA) data
-
-    # fourth, check that the order of columns in training data is the same as in testing data
-    training = pd.read_csv('../Training_Data/0_Master_Data/0_Training_Trait+Soil+Meta+Weather+EC_Data_2014_2021.csv')
-    training_cols = training.columns
-
-    testing = ...
-    testing_cols = testing.columns
-
-    training_cols.eq(testing_cols)
+    len(ts.Env.unique()) # 26
+    len(meta.index.unique()) # 26
+    len(wpc.index.unique()) # 26
+    len(ec.Env.unique()) # 24 the problem starts here!
+    len(mwe.Env.unique()) #  17 this is the problem! cannot drop nas from mwe!
+    # find which Env are missing in ec
+    wpc[~wpc.index.isin(ec.Env)] # IAH1_2022 & NCH1_2022
+    # Since these 2 are missing, we will have at least 2 Test Sets
+    # one will be the hybrids in out
+    # the rest will include the hybrids that were lost when merging soil with mwe
+    # thus, we will have to retrain models based on the common features of this second Test set
+    # to get the yield of the remaining hybrids.
+    # So, instead of "left", let's do "inner"
+    out = pd.merge(ts, mwe, how="inner", on="Env")
+    out.shape # 7180 hybrids in the first Test set
+    out.isna().sum() # cannot drop all rows with na bc it returns 0 rows. Instead I will impute this dataset
+    out.drop(['LabID', 'Date Received', 'Date Reported', 'Texture No','Texture', 'Comments'], 
+        axis=1, inplace=True) # drop columns irrelevant to imputation
+    out.to_csv("0a_Testing_Trait+Soil+Meta+Weather+EC_2022.csv", index=False) #save it!!!!
+    # Impute columns of out!!!!!!!
+    # get spearman's rho heatmap to determine columns
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    s = out.iloc[:,:25]
+    s.to_csv("../0a_Testing_Trait+Soil+Meta+Weather+EC_2022_toImpute.csv", index=False)
+    scor = s.select_dtypes("float64").corr()
+    fig, ax = plt.subplots(figsize=(20,20))
+    sns.heatmap(scor, ax=ax, annot=True)
+    plt.savefig("../0a_Testing_Trait+Soil+Meta+Weather+EC_2022_toImpute_corr.pdf")
+    plt.close()
+    # submit imputation
+    os.system("sbatch submit.sb")
+    
+    out.to_csv("0_Master_Data/0_Training_Trait+Soil+Meta+Weather+EC_Data_2014_2021.csv", index=False)
+    # Keep only columns common to training and testing data
+    # 1. generate testing set: done! see above
+    test = pd.read_csv("0a_Testing_Trait+Soil+Meta+Weather+EC_2022.csv")
+    train = pd.read_csv("../Training_Data/0_Master_Data/0_Training_Trait+Soil+Meta+Weather+EC_Data_2014_2021.csv")
+    test.shape
+    train.shape
+    # 2. generate new training and testing sets:
+    new_train = train.loc[:,train.columns.isin(test.columns)]
+    new_train.shape
+    new_test = test.loc[:,test.columns.isin(new_train.columns)]
+    new_test.shape
+    new_train.to_csv("../Training_Data/0_Master_Data/0_MASTER_Training_tsmwe_Data_2014_2021.csv", index=False)
+    new_test.to_csv("0_Master_Data/0a_MASTER_Testing_tsmwe_Data_2022.csv", index=False)
+    # 3. split new_train into training and validation sets
+    test = pd.read_csv('../Training_Data/0_Master_Data/test.txt', header=None)
+    new_train_train = new_train[~new_train.ID.isin(test[0])]
+    new_train_val = new_train[new_train.ID.isin(test[0])]
+    new_train_train.shape
+    new_train_val.shape
+    new_train_train.to_csv("../Training_Data/0_Master_Data/0_MASTER_Training_tsmwe_Data_2014_2021_TRAIN.csv", index=False)
+    new_train_val.to_csv("../Training_Data/0_Master_Data/0_MASTER_Training_tsmwe_Data_2014_2021_VAL.csv", index=False)
+    
+    # Make test set for remaining hybrids that were left out
+    # Keep only columns common to training and testing data
+    # 1. generate new training st
+    # 2. generate testing set
